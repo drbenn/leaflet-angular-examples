172d8abaac30ce542c8c846466aeaa575c4ce700
Dan Bennett <daniel.bennett@neumaconsulting.com>
2022-11-10 16:10:28 -0500

    simpler heat addressPoints

--- src/app/heat/heat.component.ts
+++ src/app/heat/heat.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import * as L from "leaflet";
 import "leaflet.heat/dist/leaflet-heat.js";
-import { addressPoints } from './realworld10000';
 
 /**
  * REQUIREMENTS
@@ -10,6 +9,8 @@ import { addressPoints } from './realworld10000';
  * npm i --save-dev @types/leaflet
  * in angular.json add to styles - "styles": ["../node_modules/leaflet/dist/leaflet.css", "styles.css","src/styles.scss"],
  */
+
+
 
 @Component({
   selector: 'app-heat',
@@ -20,21 +21,37 @@ export class HeatComponent implements OnInit {
   options = {
     layers: [
       L.tileLayer("http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png", {
-        maxZoom: 18,
+        maxZoom: 20,
         attribution: ""
       })
     ],
     zoom: 12,
-    center: L.latLng(-37.87, 175.475)
+    center: L.latLng(43.681420,-79.474883)
   };
+  
+  addressPoints = [
+    [43.681420,-79.424883,200],
+    [43.681420,-79.444883,100],
+    [43.671420,-79.424883,170],
+    [43.661420,-79.424883,2000],
+    [43.651420,-79.424883,90],
+    [43.644420,-79.425883,4000], 
+  ]
 
   onMapReady(map:any) {
-    let newAddressPoints = addressPoints.map(function (p) {
-      return [p[0], p[1]];
+    let newAddressPoints = this.addressPoints.map(function (p) {
+      return [p[0], p[1],p[2]];
     });
     const heat = (L as any)
       .heatLayer(newAddressPoints, { radius: 25 })
       .addTo(map);
+      console.log((L as any).heatLayer(newAddressPoints, 
+        { radius: 25,
+        gradient: {0.4: 'pink', 0.65: 'lime', 1.0:'white'},
+        max: 1,
+        }
+        ));
+      
   }
 
   constructor() {}
